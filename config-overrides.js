7a468056485fc225dbaef5f9d10c988003478c3a
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-11-06 21:38:46 +0330

    add example

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,54 @@ const resolvePath = (relativePath) => path.resolve(__dirname, relativePath);
 
 shimFile(
   resolvePath(
+    "./node_modules/react-native-reanimated/src/reanimated2/NativeReanimated.js"
+  ),
+  [
+    {
+      replace: 1,
+      value: "import { Platform } from 'react-native';",
+    },
+    {
+      replace: 8,
+      value:
+        "      require('react-native').TurboModuleRegistry.getEnforcing('NativeReanimated');",
+    },
+  ]
+);
+
+shimFile(
+  resolvePath(
+    "./node_modules/react-native-reanimated/src/createAnimatedComponent.js"
+  ),
+  [
+    {
+      replace: 13,
+      value: "",
+    },
+    {
+      toEnd: true,
+      value: `function setAndForwardRef({ getForwardedRef, setLocalRef }) {
+        return function forwardRef(ref) {
+          const forwardedRef = getForwardedRef();
+      
+          setLocalRef(ref);
+      
+          // Forward to user ref prop (if one has been specified)
+          if (typeof forwardedRef === 'function') {
+            // Handle function-based refs. String-based refs are handled as functions.
+            forwardedRef(ref);
+          } else if (typeof forwardedRef === 'object' && forwardedRef != null) {
+            // Handle createRef-based refs
+            forwardedRef.current = ref;
+          }
+        };
+      }`,
+    },
+  ]
+);
+
+shimFile(
+  resolvePath(
     "./node_modules/react-scripts/scripts/utils/verifyTypeScriptSetup.js"
   ),
   [
@@ -23,6 +71,9 @@ const srcIncludes = [
 // our packages that will now be included in the CRA build step
 const appIncludes = [
   ...srcIncludes,
+  resolvePath("./node_modules/react-native-animatable"),
+  resolvePath("./node_modules/react-native-reanimated"),
+  resolvePath("./node_modules/react-native-redash"),
   resolvePath("./node_modules/react-native-gesture-handler"),
 ];
 
@@ -37,6 +88,10 @@ module.exports = function override(config, env) {
   config.module.rules[0].include = appIncludes;
 
   config.module.rules[1].oneOf[2].include = appIncludes;
+  config.module.rules[1].oneOf[2].options.plugins = [
+    "react-native-reanimated/plugin",
+  ].concat(config.module.rules[1].oneOf[2].options.plugins);
+  config.module.rules = config.module.rules.filter(Boolean);
 
   config.plugins.push(new webpack.DefinePlugin({ __DEV__ }));
   return config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,46 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import Bouncing from "./bouncing";
+import SwipableList from "./SwipeableListExample"
 import { PanGestureHandler } from "react-native-gesture-handler";
 import "./App.css";
 
 function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "yellow",
-        overflow: "hidden",
-      }}
-    >
-      <PanGestureHandler
-        onGestureEvent={(gestureEvent) => {
-          console.log({ gestureEvent });
-        }}
-        onHandlerStateChange={(stateEvent) => {
-          console.log({ stateEvent });
+    <>
+      <Text>Log PanGestureHandler </Text>
+
+      <View
+        style={{
+          backgroundColor: "yellow",
+          overflow: "hidden",
         }}
       >
-        <View
-          style={{
-            backgroundColor: "blue",
-            height: 100,
-            width: 200,
-            margin: 20,
-            alignItems: "center",
-            justifyContent: "center",
+        <PanGestureHandler
+          onGestureEvent={(gestureEvent) => {
+            console.log({ gestureEvent });
           }}
-        ></View>
-      </PanGestureHandler>
-    </View>
+          onHandlerStateChange={(stateEvent) => {
+            console.log({ stateEvent });
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "blue",
+              height: 100,
+              width: 200,
+              margin: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></View>
+        </PanGestureHandler>
+      </View>
+      <Text> Bouncing </Text>
+      <Bouncing />
+      <Text> Reanimated 2 SwipableList </Text>
+      <SwipableList />
+    </>
   );
 }
 
